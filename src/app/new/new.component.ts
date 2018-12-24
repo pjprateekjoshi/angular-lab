b3c687d2670840498a3480ff78417ecf05147939
Prateek Joshi <pjprateekjoshi@gmail.com>
2018-12-25 03:17:36 +0530

    implementing si using pb on new comp

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponent implements OnInit {
 
+  writeInDiv = "I'm property binding to implement String interpolation";
   isDisabled = false;
   constructor() {
     setTimeout(()=>{
       this.isDisabled = !this.isDisabled
-    },3000)
+    },3000);
   }
   ngOnInit() {
   }
